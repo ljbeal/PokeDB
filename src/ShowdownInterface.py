2fb746cfb6697f57957afdcdd008130fcff881bf
ljbeal <louis.j.beal@gmail.com>
2023-12-16 17:13:15 +0100

    json data is now extracted from typescript constants

--- src/ShowdownInterface.py
+++ src/ShowdownInterface.py
@@ -5,35 +5,88 @@ Learnset documentation can be found here:
 https://github.com/smogon/pokemon-showdown/blob/master/sim/global-types.ts
 """
 import json
-import pathlib
 
-import requests
+import yaml
 
 from src import package_root
 
 
-URLS = ["https://play.pokemonshowdown.com/data/pokedex.json",
-        "https://play.pokemonshowdown.com/data/learnsets.json"]
+ROOT = package_root()
 
 
-def collect_data(url) -> None:
+def collect_data(file: str) -> dict:
     """
-    Collect the showdown database and dump to json
+    Extract Information from the showdown typescript files
     """
-    raw = requests.get(url).json()
+    with open(file) as o:
+        raw = o.readlines()
 
-    name = url.split("/")[-1]
+    # drop everything up until the actual "export const ..."
+    i = 0
+    for i, line in enumerate(raw):
+        if line.startswith("export"):
+            break
+    raw = raw[i:]
 
-    outfile = package_root() / pathlib.Path(f"jsondata/{name}")
+    # cut out the typescript constant definition
+    raw[0] = f"{{\n"
 
-    print(f"Dumping data to {outfile}", end="... ")
-    with open(outfile, "w+") as o:
-        json.dump(raw, o, indent=2)
+    # fix formatting to convert to valid YAML
+    parsed = []
+    code_block = False
+    code_start_indent = -1
 
-    print("Done.")
+    for line in raw:
+        indent = line.count("\t")
+
+        if not code_block:
+            code_block = (":" not in line and
+                          "}" not in line and
+                          "{" in line and
+                          line.strip() != "{")
+
+        if not code_block and "condition:" in line:
+            code_block = True
+
+        # print(indent, code_start_indent, code_block, line.strip())
+
+        if code_block and code_start_indent == -1:
+            parsed.append(f"{'  ' * indent}callback: True,\n")
+            code_start_indent = indent
+            continue
+        elif code_block and "}," in line.strip() and indent == code_start_indent:
+            code_start_indent = -1
+            code_block = False
+            # parsed.append("  " * indent + "},\n")
+            continue
+        elif code_block:
+            continue
+
+        line = line.replace("\t", "  ")
+        line = line.replace("//", "#")
+
+        parsed.append(line)
+
+    # drop the ending semicolon and we now have valid yaml
+    parsed = "".join(parsed)[:-2]
+
+    with open("debug.txt", "w+") as o:
+        o.write(parsed)
+
+    data = yaml.safe_load(parsed)
+
+    return data
 
 
 if __name__ == "__main__":
-    for url in URLS:
-        print(f"collecting url {url}")
-        collect_data(url)
+    data = collect_data(ROOT / "pokemon-showdown/data/pokedex.ts")
+    with open(ROOT / "jsondata/pokedex.json", "w+") as o:
+        json.dump(data, o, indent = 2)
+
+    data = collect_data(ROOT / "pokemon-showdown/data/moves.ts")
+    with open(ROOT / "jsondata/moves.json", "w+") as o:
+        json.dump(data, o, indent = 2)
+
+    data = collect_data(ROOT / "pokemon-showdown/data/learnsets.ts")
+    with open(ROOT / "jsondata/learnsets.json", "w+") as o:
+        json.dump(data, o, indent = 2)
